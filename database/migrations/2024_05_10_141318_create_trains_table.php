5071233066e492d9e4256d807b3d5ef478579ac7
GioMench <giorgiaglm@gmail.com>
2024-05-10 16:24:45 +0200

    create migrate of train table

---
+++ database/migrations/2024_05_10_141318_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',20);
+            $table->string('stazione_di_partenza',50);
+            $table->string('stazione_di_arrivo',50);
+            $table->time('orario_di_partenza');
+            $table->integer('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
+            
+
+            
+            
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
